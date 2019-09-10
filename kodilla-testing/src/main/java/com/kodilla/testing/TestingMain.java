1228b75ec3ad4bac09bcec55eb7a5e8cde6472d8
prpsolutions <info@prpsolutions.eu>
2019-09-10 10:55:10 +0200

    Modul 6 - wprowadzenie do testowania

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -0,0 +1,9 @@
+package com.kodilla.testing;
+
+import java.sql.SQLOutput;
+
+public class TestingMain {
+    public static void main(String [] args){
+        System.out.println("Modul 6. Wprowadzenie do testowania oprogramowania");
+    }
+}
