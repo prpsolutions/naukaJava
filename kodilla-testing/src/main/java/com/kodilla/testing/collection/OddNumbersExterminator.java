e82d83ea3854314fd95adef39fa426c774f8c330
prpsolutions <info@prpsolutions.eu>
2019-10-07 00:55:27 +0200

    zadanie 6.3

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -7,26 +7,27 @@ public class OddNumbersExterminator {
     private ArrayList<Integer> listaParzysta;
     private ArrayList<Integer> listaNieparzysta;
 
-    public OddNumbersExterminator(){
+    public OddNumbersExterminator() {
         listaParzysta = new ArrayList<Integer>();
         listaNieparzysta = new ArrayList<Integer>();
     }
 
-     public ArrayList<Integer> getParzysteList() {
+    public ArrayList<Integer> getParzysteList() {
         return listaParzysta;
     }
-    public ArrayList<Integer> getNieparzysteList(){
+
+    public ArrayList<Integer> getNieparzysteList() {
         return listaNieparzysta;
     }
 
-    public ArrayList<Integer> exterminate(List<Integer> numbers) {
+    public void exterminate(List<Integer> numbers) {
 
         for (Integer f : numbers) {
             if (f % 2 == 0) {
                 listaParzysta.add(f);
-            } else {listaNieparzysta.add(f);
+            } else {
+                listaNieparzysta.add(f);
             }
         }
-        return new ArrayList<>(listaParzysta);
     }
 }
